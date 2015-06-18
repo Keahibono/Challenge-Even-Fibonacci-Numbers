a18bbc7fe589d2c091ccff1cf355dbc0bed1370a
Paul Selhorst <paulselhorst@Pauls-MacBook-Pro.local>
2015-06-17 18:55:47 -1000

    Finally

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -1,6 +1,12 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
   var sum = 0;
   // do your work here
-  
+  for(var i = 0 , j = 1 , k = 0; k < n ; i = j , j = x , k++){
+        var x = i + j;
+
+        if (x % 2 === 0){
+        	sum += x;
+        }
+    }
   return sum;
 };
